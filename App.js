88a3b7b66a1bbeac0e586406ff666ca508c9d681
Lilla624 <vargalilla@ktch.hu>
2024-12-19 11:22:31 +0100

    Kész

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
 
 export default function App() {
+  const [side, setSide] = useState('');
+  const [height, setHeight] = useState('');
+  const [volume, setVolume] = useState('');
+
+  function startCalc() {
+    const res = 1.0/3.0*Math.pow(side, 2) * height;
+    setVolume(res);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Gula térfogata</Text>
+      <Text>Oldal</Text>
+      <TextInput
+      style={styles.input}
+      onChangeText={side => setSide(side)}
+      />
+      <Text>Magasság</Text>
+      <TextInput
+      style={styles.input}
+      onChangeText={height => setHeight(height)}
+      />
+      <Button
+      onPress={startCalc}
+      title="Számít"
+      />
+      <Text>Térfogat</Text>
+      <TextInput
+      style={styles.input}
+      value={volume}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +47,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    borderWidth: 1,
+    borderColor: 'green',
+    borderStyle: 'solid',
+  }
 });
